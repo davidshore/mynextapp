af6c7b26dfd6ad748b70b39cd9e0202dc570822f
David Shore <david@foggymedia.com>
2023-04-12 10:42:41 +0200

    added mysql connection

---
+++ src/lib/db.js
@@ -0,0 +1,22 @@
+import mysql from "serverless-mysql";
+
+const db = mysql({
+  config: {
+    host: process.env.DB_HOST,
+    database: "swedbank",
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
+});
+
+console.log("user", process.env.DB_USERNAME);
+
+export default async function excuteQuery({ query, values }) {
+  try {
+    const results = await db.query(query, values);
+    await db.end();
+    return results;
+  } catch (error) {
+    return { error };
+  }
+}
---
+++ src/pages/api/users.js
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import excuteQuery from "../../lib/db";
+
+export default async function handler(req, res) {
+  const sql = {
+    query: "SELECT * from USERS",
+  };
+
+  const result = await excuteQuery(sql);
+  res.status(200).json(result);
+}
